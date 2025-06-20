b86c2610d985b4de6032e18268ebaa3d25b6531e
Awadhy Mohammed <awadhym@outlook.com>
2025-06-20 21:54:45 +0100

    Create seperate enum for RED and WHITE which are only used for feedback

---
+++ source/feedbackPeg.cpp
@@ -0,0 +1,25 @@
+#include "../include/feedbackPeg.h"
+#include <algorithm>
+#include <stdexcept>
+
+const std::string pegToString(feedbackPeg peg) {
+  switch (peg) {
+  case feedbackPeg::White:
+    return "White";
+  case feedbackPeg::Red:
+    return "Red";
+  default:
+    return "Unknown";
+  }
+}
+
+const std::string toAnsi(feedbackPeg peg) {
+  switch (peg) {
+  case feedbackPeg::White:
+    return "\033[97m●\033[0m";
+  case feedbackPeg::Red:
+    return "\033[31m●\033[0m";
+  default:
+    return " ";
+  }
+}
