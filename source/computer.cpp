81622ea8e6e3729b832895659624a623cfc6e3a1
Awadhy Mohammed <awadhym@outlook.com>
2025-06-21 17:45:48 +0100

    General Improvement - Replace use of std::vector with std::array

--- source/computer.cpp
+++ source/computer.cpp
@@ -6,8 +6,8 @@ Computer::Computer(int codeLength) : codeLength(codeLength) {
   code = generateCode();
 }
 
-std::vector<CodePeg> Computer::generateCode() {
-  std::vector<CodePeg> generatedCode(codeLength);
+std::array<CodePeg, 4> Computer::generateCode() {
+  std::array<CodePeg, 4> generatedCode{};
   for (auto &slot : generatedCode) {
     slot = Computer::gen();
   }
@@ -19,14 +19,4 @@ CodePeg Computer::gen() {
   std::uniform_int_distribution<> uid(0, 3);
   CodePeg val = static_cast<CodePeg>(uid(rd));
   return val;
-}
-
-void Computer::printCode() const {
-  for (int i = 0; i < code.size(); i++) {
-    if (i == code.size() - 1) {
-      std::cout << pegToString(code[i]) << std::endl;
-    } else {
-      std::cout << pegToString(code[i]) << ",";
-    }
-  }
 }
