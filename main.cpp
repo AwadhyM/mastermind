2c2575338c7f235f925ac75a15be4fa13874739b
Awadhy Mohammed <awadhym@outlook.com>
2025-07-06 21:33:49 +0100

    Clean up main function

--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
-#include "include/board.h"
-#include "include/game.h"
 #include <iostream>
+#include "include/game.h"
 
 int main() {
-  Game game = Game();
+  Game game;
 
   std::string userChoice = game.getPromptToStartGame();
 
-  if (userChoice == "Y") {
-    auto res = game.play();
-  } else {
+  if (userChoice != "Y") {
     std::cout << "\nProgramme terminated." << std::endl;
+    return 0;
   }
 
+  Game::GameResult res = game.play();
+
   if (res == Game::GameResult::PLAYER_WON) {
-    std::cout << "Congratulation you guessed the code correctly" << std::endl;
+    std::cout << "Congratulations! You guessed the code correctly." << std::endl;
   } else {
-    std::cout << "You failed to guess the code better luck next time.."
-              << std::endl;
+    std::cout << "You failed to guess the code. Better luck next time..." << std::endl;
   }
+
+  return 0;
 }
